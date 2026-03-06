97d589152dcec01e40b7c5c1b37eb8cb92dc4936
eswar213006 <eswartunuguntla@gmail.com>
2026-03-06 10:09:26 +0530

    Initial commit

---
+++ u1.java
@@ -0,0 +1,17 @@
+
+
+class u1{
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     Welcome to Palindrome Checker App");
+        System.out.println("=================================");
+
+     
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Application Version : 1.0");
+
+        System.out.println("Program is ready to check palindromes.");
+    }
+}
