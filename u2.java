2aca1eaa32cb8a779c2a78b1c7c5f5dd340232e8
eswar213006 <eswartunuguntla@gmail.com>
2026-03-06 10:18:32 +0530

    u2

---
+++ u2.java
@@ -0,0 +1,25 @@
+public 
+class u2 {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reverse = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        // Check palindrome condition
+        if (word.equals(reverse)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+} {
+    
+}
